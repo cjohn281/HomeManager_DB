613eac03455b3cdb2e9f3c7d0594f7d79dc2105c
cjohn281 <cjohn281@outlook.com>
2026-07-12 16:52:30 -0700

    Add tblRole and tblSystemPersonRole

---
+++ dbo/Tables/tblRole.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tblRole] (
+    [rleID]               INT              IDENTITY (1, 1) NOT NULL,
+    [rleName]             VARCHAR (300)    NOT NULL,
+    [rleDateCreated]      DATETIME         DEFAULT ([dbo].[fncGetLocalDateTime]()) NULL,
+    [rleCreatedBy_prnID]  INT              NULL,
+    [rleDateModified]     DATETIME         NULL,
+    [rleModifiedBy_prnID] INT              NULL,
+    [rleGUID]             UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([rleID] ASC),
+    CONSTRAINT [tblRole_rleCreatedBy_prnID_tblPerson] FOREIGN KEY ([rleCreatedBy_prnID]) REFERENCES [dbo].[tblPerson] ([prnID]),
+    CONSTRAINT [tblRole_rleModifiedBy_prnID_tblPerson] FOREIGN KEY ([rleModifiedBy_prnID]) REFERENCES [dbo].[tblPerson] ([prnID])
+);
+
---
+++ dbo/Tables/tblSystemPersonRole.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[tblSystemPersonRole] (
+    [sprID]               INT              IDENTITY (1, 1) NOT NULL,
+    [sprSystem_sysID]     INT              NOT NULL,
+    [sprPerson_prnID]     INT              NOT NULL,
+    [sprRole_rleID]       INT              NOT NULL,
+    [sprDateCreated]      DATETIME         DEFAULT ([dbo].[fncGetLocalDateTime]()) NULL,
+    [sprCreatedBy_prnID]  INT              NULL,
+    [sprDateModified]     DATETIME         NULL,
+    [sprModifiedBy_prnID] INT              NULL,
+    [sprGUID]             UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([sprID] ASC),
+    CONSTRAINT [fk_tblSystemPersonRole_sprCreatedBy_prnID_tblPerson] FOREIGN KEY ([sprCreatedBy_prnID]) REFERENCES [dbo].[tblPerson] ([prnID]),
+    CONSTRAINT [fk_tblSystemPersonRole_sprModifiedBy_prnID_tblPerson] FOREIGN KEY ([sprModifiedBy_prnID]) REFERENCES [dbo].[tblPerson] ([prnID]),
+    CONSTRAINT [fk_tblSystemPersonRole_sprPerson_prnID_tblPerson] FOREIGN KEY ([sprPerson_prnID]) REFERENCES [dbo].[tblPerson] ([prnID]),
+    CONSTRAINT [fk_tblSystemPersonRole_sprRole_rleID_tblRole] FOREIGN KEY ([sprRole_rleID]) REFERENCES [dbo].[tblRole] ([rleID]),
+    CONSTRAINT [fk_tblSystemPersonRole_sprSystem_sysID_tblSystem] FOREIGN KEY ([sprSystem_sysID]) REFERENCES [dbo].[tblSystem] ([sysID]),
+    CONSTRAINT [UQ_tblSystemPersonRole] UNIQUE NONCLUSTERED ([sprSystem_sysID] ASC, [sprPerson_prnID] ASC)
+);
+
